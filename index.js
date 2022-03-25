651db390cc6e991e51b8a43fe1adfc62976b4653
Joao Witor Felipe <joaowitorfelipe2@gmail.com>
2022-03-24 22:59:48 -0300

    add simple cookie auth logic between services

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require('express')();
 const proxy = require('express-http-proxy');
+const axios = require('axios');
 
 const { USERS_SERVICE, PRODUCTS_SERVICE } = require('./available_services.js');
 
@@ -7,8 +8,24 @@ const userServiceProxy = proxy(USERS_SERVICE);
 const productServiceProxy = proxy(PRODUCTS_SERVICE);
 
 app.get('/', (req, res) => res.send("API GATEWAY"));
+
 app.get('/users', (req, res, next) => userServiceProxy(req, res, next));
-app.get('/products', (req, res, next) => productServiceProxy(req, res, next));
+app.post('/users/authentication', userServiceProxy);
+
+
+app.get('/products', async (req, res, next) => {
+    
+    const isAuthResponse = await axios.get(USERS_SERVICE + "/users/is-authenticated", {
+        headers: req.headers
+    });
+
+    if (isAuthResponse.data.ok) {
+        return productServiceProxy(req, res, next)
+    }
+
+    return res.send('without_permission');
+
+});
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`API Gateway started in port ${PORT}`));
